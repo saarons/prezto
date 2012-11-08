a8532f1e56eaad25912e3274ba298a9842ada06d
Sam Aarons <samaarons@gmail.com>
2012-11-08 17:47:27 -0500

    Add my dotfiles

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+setopt EXTENDED_GLOB
+cd "${ZDOTDIR:-$HOME}"
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
