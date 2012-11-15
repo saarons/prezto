e0451c1618adaf585cc765a2bcb4999d3819c5dc
Sam Aarons <samaarons@gmail.com>
2012-11-15 02:09:49 -0500

    Force

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 setopt EXTENDED_GLOB
 cd "${ZDOTDIR:-$HOME}"
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  ln -s -f "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
