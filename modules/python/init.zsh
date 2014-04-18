70ebaf2c3892859797a912e7688c0d8bef3fd1bb
Sam Aarons <samaarons@gmail.com>
2014-04-18 18:19:18 -0400

    Cool

--- modules/python/init.zsh
+++ modules/python/init.zsh
@@ -32,15 +32,7 @@ if (( ! $+commands[python] && ! $+commands[pyenv] )); then
 fi
 
 # Load virtualenvwrapper into the shell session.
-if (( $+commands[virtualenvwrapper_lazy.sh] )); then
-  # Set the directory where virtual environments are stored.
-  export WORKON_HOME="$HOME/.virtualenvs"
-
-  # Disable the virtualenv prompt.
-  VIRTUAL_ENV_DISABLE_PROMPT=1
-
-  source "$commands[virtualenvwrapper_lazy.sh]"
-fi
+export VIRTUAL_ENV_DISABLE_PROMPT=1
 
 #
 # Aliases
